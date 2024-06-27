1d9fdafd79665e6c50fe304ff9f4840b1e500fdf
Pepi10 <anabiotic1@gmail.com>
2024-06-27 17:10:12 +0900

    setting

--- env_install/8_run_kubeflow.sh
+++ env_install/8_run_kubeflow.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+minikube start --gpus nvidia --memory=8192 --cpus=16
+
+minikube kubectl -- get pods -A
+
 echo "초기 실행에 30분정도 소요됩니다."
 echo "초기 실행에 30분정도 소요됩니다."
 echo "초기 실행에 30분정도 소요됩니다."
@@ -9,3 +13,8 @@ echo "초기 실행에 30분정도 소요됩니다."
 cd ../manifests/
 
 while ! kustomize build example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
+
+cd ../https_setting/
+
+k apply -f certificate.yaml
+k apply -f gateway.yaml
