615e46cadf47b15c00e1fb259ea178efbd5a0c22
Pepi10 <anabiotic1@gmail.com>
2024-06-28 23:15:24 +0900

    debug

--- env_install/1_install_cudnn.sh
+++ env_install/1_install_cudnn.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
+
+set -e
+
 sudo apt-get -y update
 sudo apt-get -y remove --purge 'cuda-.*'
 sudo apt-get -y install nvidia-cuda-toolkit
+
 nvcc -V
 whereis cuda
-mkdir ~/nvidia
+
+mkdir -p ~/nvidia
 cd ~/nvidia
 
-CUDNN_TAR_FILE="cudnn-linux-x86_64-8.3.3.40_cuda11.5-archive.tar.xz"
-wget https://developer.download.nvidia.com/compute/redist/cudnn/v8.3.3/local_installers/11.5/${CUDNN_TAR_FILE}
-tar -xf ${CUDNN_TAR_FILE}
-mv cudnn-linux-x86_64-8.3.3.40_cuda11.5-archive cuda
+CUDNN_VERSION="8.3.3.40"
+CUDA_VERSION="11.5"
+CUDNN_TAR_FILE="cudnn-linux-x86_64-${CUDNN_VERSION}_cuda${CUDA_VERSION}-archive.tar.xz"
 
-sudo cp cuda/include/cudnn*.h /usr/lib/cuda/include/
-sudo cp cuda/lib/libcudnn* /usr/lib/cuda/lib64/
-sudo chmod a+r /usr/lib/cuda/lib64/libcudnn*
-sudo chmod a+r /usr/lib/cuda/include/cudnn*.h
+wget https://developer.download.nvidia.com/compute/redist/cudnn/v${CUDNN_VERSION}/local_installers/${CUDA_VERSION}/${CUDNN_TAR_FILE}
+tar -xf ${CUDNN_TAR_FILE}
+mv cudnn-linux-x86_64-${CUDNN_VERSION}_cuda${CUDA_VERSION}-archive cuda
+
+sudo cp cuda/include/cudnn*.h /usr/local/cuda/include/
+sudo cp cuda/lib/libcudnn* /usr/local/cuda/lib64/
+sudo chmod a+r /usr/local/cuda/lib64/libcudnn*
+sudo chmod a+r /usr/local/cuda/include/cudnn*.h
 
 echo "export LD_LIBRARY_PATH=/usr/lib/cuda/lib64:$LD_LIBRARY_PATH" >> ~/.bashrc
 export "LD_LIBRARY_PATH=/usr/lib/cuda/include:$LD_LIBRARY_PATH" >> ~/.bashrc
 source ~/.bashrc
 
-sudo apt-get install -y nvidia-driver-550
-echo "reboot....."
+sudo apt-get install -y nvidia-driver-460
+
+echo "Please reboot system"
--- env_install/7_cluster_setting.sh
+++ env_install/7_cluster_setting.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-minikube start --gpus nvidia --memory=8192 --cpus=16
+minikube start --gpus nvidia --memory=3072 --cpus=2
 
 minikube kubectl -- get pods -A
 
