3a63ece73528ea93d96bddca13a7fae499e8706c
Pepi10 <anabiotic1@gmail.com>
2024-06-29 15:01:01 +0900

    organized

--- env_install/8_run_kubeflow.sh
+++ env_install/8_run_kubeflow.sh
@@ -9,7 +9,7 @@ cd ../manifests/
 
 while ! kustomize build example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
 
-cd ../https_setting/
+cd ../env_install/https_setting/
 
 kubectl apply -f certificate.yaml
 kubectl apply -f gateway.yaml
