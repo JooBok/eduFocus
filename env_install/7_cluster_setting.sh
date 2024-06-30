938f9abc811de227e9f65428ea0e724116733c28
Pepi10 <anabiotic1@gmail.com>
2024-06-30 19:46:45 +0900

    cluster setting

--- env_install/7_cluster_setting.sh
+++ env_install/7_cluster_setting.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-minikube start --gpus nvidia --memory=3072 --cpus=2
+### minikube gpu 실행과 스탠다드 스토리지 클래스를 생성하는 스크립트입니다. ###
+### 컴퓨터 사양에 맞게 재구성해주세요 ###
+minikube start --gpus nvidia --memory=10240 --cpus=8
 
 minikube kubectl -- get pods -A
 
