3726b768232ecfcb9be64fad6aadfda404e4bd2d
Pepi10 <anabiotic1@gmail.com>
2024-06-30 19:45:02 +0900

    docker

--- env_install/2_install_docker.sh
+++ env_install/2_install_docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+### docker 최신버전 다운로드 스크립트입니다. ###
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
