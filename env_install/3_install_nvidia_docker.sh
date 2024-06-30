815311429afabb8bb3d176f0d92e418fabdf5180
Pepi10 <anabiotic1@gmail.com>
2024-06-30 19:45:21 +0900

    nvidia-docker

--- env_install/3_install_nvidia_docker.sh
+++ env_install/3_install_nvidia_docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+### docker nvidia 최신버전 다운로드 스크립트입니다. ###
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
