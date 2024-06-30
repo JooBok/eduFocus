f28ae403d3bd170c12857c030b5212fecb8ad8a8
Pepi10 <anabiotic1@gmail.com>
2024-06-30 19:46:24 +0900

    cluster

--- env_install/5_minikube.sh
+++ env_install/5_minikube.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
+### minikube 최신버전 다운로드 스크립트 입니다. (공식문서 참조) ### 
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
 echo 'alias kubectl="minikube kubectl --"' >> ~/.bashrc
-alias kubectl="minikube kubectl --"
 
 source ~/.bashrc
 
