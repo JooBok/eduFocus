fb6b336ecc811432b6d86165fd7788dc10b323c6
Pepi10 <anabiotic1@gmail.com>
2024-07-10 08:57:34 +0900

    example

--- jaekyeong/example/mediapipe_example/eye_tracking.py
+++ jaekyeong/example/mediapipe_example/eye_tracking.py
@@ -4,11 +4,10 @@ import numpy as np
 import joblib
 from scipy.spatial.transform import Rotation
 import time
-from kafka import KafkaConsumer
-import requests
 import json
-### docker image
-### kafka consumer로 mediapipe 사용
+from kafka import KafkaConsumer
+import asyncio
+import aiohttp
 
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(
@@ -18,22 +17,11 @@ face_mesh = mp_face_mesh.FaceMesh(
 )
 mp_drawing = mp.solutions.drawing_utils
 
-### 모델 로드 ###
+### model Load ###
 model_x = joblib.load('model_x.pkl')
 model_y = joblib.load('model_y.pkl')
 
-### Kafka ###
-consumer = KafkaConsumer('kid_face_video', bootstrap_servers=['kafka-service:9092'])
-aggregator_url = "http://result-aggregator-service/aggregate"
-
-### 기존 mediapipe ###
-######################################################################################################################################
-
 class GazeBuffer:
-    """
-    안정적인 결과를 얻기 위하여 이전 프레임 시선 데이터와의 연계
-    $ buffer_size: 몇 프레임 전까지 저장하여 평균을 낼 것인가를 정함
-    """
     def __init__(self, buffer_size=3):
         self.buffer = []
         self.buffer_size = buffer_size
@@ -49,12 +37,6 @@ class GazeBuffer:
         return np.mean(self.buffer, axis=0)
 
 class GazeFixation:
-    """
-    시선좌표와 시간데이터를 저장하여 속도를 측정하여 fixation 설정
-    $ velocity_threshold: 평균 속도가 이 값보다 낮을 때, 시선이 고정된 것으로 간주 (픽셀/s)
-    $ duration: 시선이 고정된 상태로 인식되기 위해 요구되는 최소 지속 시간
-    $ window_size: 이전 프레임과의 연계 (프레임 개수)
-    """
     def __init__(self, velocity_threshold=0.1, duration=0.2, window_size=6):
         self.velocity_threshold = velocity_threshold
         self.duration = duration
@@ -62,6 +44,7 @@ class GazeFixation:
         self.gaze_history = []
         self.time_history = []
         self.start_time = None
+
     def update(self, gaze):
         current_time = time.time()
         self.gaze_history.append(gaze)
@@ -72,11 +55,13 @@ class GazeFixation:
 
         if len(self.gaze_history) < self.window_size:
             return False
+        
         velocities = []
         for i in range(1, len(self.gaze_history)):
             dist = np.linalg.norm(np.array(self.gaze_history[i]) - np.array(self.gaze_history[i-1]))
             time_diff = self.time_history[i] - self.time_history[i-1]
             velocities.append(dist / time_diff if time_diff > 0 else 0)
+        
         avg_velocity = np.mean(velocities)
         if avg_velocity < self.velocity_threshold:
             if self.start_time is None:
@@ -86,6 +71,12 @@ class GazeFixation:
         else:
             self.start_time = None
         return False 
+
+gaze_buffer = GazeBuffer()
+gaze_fixation = GazeFixation()
+gaze_sequence = []
+sequence_length = 10
+prev_gaze = None
 
 def calculate_distance(iris_landmarks, image_height):
     left_iris, right_iris = iris_landmarks
@@ -106,16 +97,18 @@ def estimate_head_pose(face_landmarks):
     left_eye = np.array([face_landmarks.landmark[33].x, face_landmarks.landmark[33].y, face_landmarks.landmark[33].z])
     right_eye = np.array([face_landmarks.landmark[263].x, face_landmarks.landmark[263].y, face_landmarks.landmark[263].z])
     face_normal = np.cross(right_eye - nose, left_eye - nose)
-    face_normal /= np.linalg.norm(face_normal)
-    rotation_matrix = Rotation.align_vectors([[0, 0, -1]], [face_normal])[0].as_matrix()
+    if np.linalg.norm(face_normal) > 1e-6:
+        face_normal = face_normal / np.linalg.norm(face_normal)
+        rotation_matrix = Rotation.align_vectors([[0, 0, -1]], [face_normal])[0].as_matrix()
+    else:
+        rotation_matrix = np.eye(3) 
     return rotation_matrix
 
 def correct_gaze_vector(gaze_vector, head_rotation):
     corrected_gaze = np.dot(head_rotation, gaze_vector)
     return corrected_gaze
 
-### $ 프레임당 시선좌표 이동속도 제한 ###
-def filter_sudden_changes(new_gaze, prev_gaze, max_change_x=10, max_change_y=10):
+def filter_sudden_changes(new_gaze, prev_gaze, max_change_x=15, max_change_y=15):
     if prev_gaze is None:
         return new_gaze
     change_x = abs(new_gaze[0] - prev_gaze[0])
@@ -126,40 +119,29 @@ def filter_sudden_changes(new_gaze, prev_gaze, max_change_x=10, max_change_y=10)
         new_gaze[1] = prev_gaze[1] + (new_gaze[1] - prev_gaze[1]) * (max_change_y / change_y)
     return new_gaze
 
-### 시선이 화면 밖으로 나가지 않도록 제한하는 함수 ###
 def limit_gaze_to_screen(gaze_point_x, gaze_point_y, screen_width, screen_height):
     gaze_point_x = min(max(gaze_point_x, 0), screen_width - 1)
     gaze_point_y = min(max(gaze_point_y, 0), screen_height - 1)
     return gaze_point_x, gaze_point_y
 
-gaze_buffer = GazeBuffer()
-gaze_fixation = GazeFixation()
-gaze_sequence = []
-sequence_length = 10
-prev_gaze = None    
+### 비동기 frame 처리 ###
+async def process_frame(frame, frame_number):
+    global gaze_buffer, gaze_fixation, gaze_sequence, prev_gaze
 
-######################################################################################################################################
+    image = cv2.flip(frame, 1)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = face_mesh.process(image_rgb)
 
-def process_frame(frame):
+    h, w = image.shape[:2]
 
-    results = face_mesh.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-        
-    w, h = frame[:2]
-    
     if results.multi_face_landmarks:
         for face_landmarks in results.multi_face_landmarks:
-            mp_drawing.draw_landmarks(
-                results, face_landmarks, mp_face_mesh.FACEMESH_TESSELATION,
-                mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=1, circle_radius=1),
-                mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=1)
-            )
-
             left_iris = get_center(face_landmarks.landmark[468:474])[:3]
             right_iris = get_center(face_landmarks.landmark[473:479])[:3]
             left_eye = get_center(face_landmarks.landmark[33:42])[:3]
             right_eye = get_center(face_landmarks.landmark[263:272])[:3]
 
-            estimated_distance = calculate_distance([left_iris, right_iris], results.shape[0])
+            estimated_distance = calculate_distance([left_iris, right_iris], image.shape[0])
 
             left_gaze = estimate_gaze(left_eye, left_iris, estimated_distance)
             right_gaze = estimate_gaze(right_eye, right_iris, estimated_distance)
@@ -176,26 +158,16 @@ def process_frame(frame):
                 gaze_sequence.pop(0)
 
             if len(gaze_sequence) == sequence_length:
-                ### flatten ###
                 gaze_input = np.array(gaze_sequence).flatten().reshape(1, -1)
-                ### 모델 예측 ###
                 predicted_x = model_x.predict(gaze_input)[0]
                 predicted_y = model_y.predict(gaze_input)[0]
 
-                ### 평활화 ###
                 gaze_buffer.add(np.array([predicted_x, predicted_y]))
                 smoothed_gaze = gaze_buffer.get_average()
 
-                ### 노이즈 필터링 ###
-                filtered_gaze = filter_sudden_changes(
-                    new_gaze = smoothed_gaze, 
-                    prev_gaze = prev_gaze,
-                    max_change_x = 10,
-                    max_change_y = 10
-                    )
+                filtered_gaze = filter_sudden_changes(smoothed_gaze, prev_gaze)
 
                 predicted_x, predicted_y = filtered_gaze
-                ### previous gaze 갱신 ###
                 prev_gaze = filtered_gaze
 
                 screen_x = int((predicted_x + 1) * w / 2)
@@ -204,28 +176,61 @@ def process_frame(frame):
                 screen_x, screen_y = limit_gaze_to_screen(screen_x, screen_y, w, h)
                 screen_x, screen_y = int(screen_x), int(screen_y)
 
-                is_fixed = gaze_fixation.update((screen_x, screen_y))
-
-                return {"gaze_x": screen_x, "gaze_y": screen_y, "is_fixed": is_fixed}
+                return {
+                    "frame_number": frame_number,
+                    "gaze_point": [screen_x, screen_y]
+                }
 
     return None
 
-def process_video_data(video_data):
-    ### 비디오 데이터를 프레임으로 변환 ###
-    nparr = np.frombuffer(video_data, np.uint8)
-    frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    
-    ### 프레임 처리 ###
-    result = process_frame(frame)
-    
-    return result
+### result-aggregator 엔드포인트에 결과값 제출 ###
+async def send_result(session, result):
+    async with session.post("http://result-aggregator-service/aggregate", json=result) as response:
+        return await response.text()
 
-############################## 메인 루프 ##############################
-for message in consumer:
-    video_data = message.value
+### 결과값 계산 함수 ###
+def calc(temp_data, saliency_map):
+    count = 0
+    total_frames = len(temp_data)
+    for frame_data in temp_data.values():
+        x, y = frame_data
+        if saliency_map[y][x] > 0.7:
+            count += 1
+    res = count / total_frames
+    return res
+
+### kafka producer에서 토픽을 통해 frame 가져오기 ###
+async def process_stream():
+    consumer = KafkaConsumer('kid_face_video', bootstrap_servers=['kafka-service:9092'])
     
-    ### MediaPipe 처리 ###
-    result = process_video_data(video_data)
+    temp_data = {}
+    frame_number = 0
     
-    ### 결과를 aggregator로 전달 ###
-    requests.post(aggregator_url, json=json.dumps(result))
+    async with aiohttp.ClientSession() as session:
+        for message in consumer:
+            frame = cv2.imdecode(np.frombuffer(message.value, np.uint8), cv2.IMREAD_COLOR)
+            result = await process_frame(frame, frame_number)
+            if result:
+                temp_data[frame_number] = result['gaze_point']
+            frame_number += 1
+
+            if video_end(message):
+                break
+
+        saliency_map = load_saliency_map()
+        final_result = calc(temp_data, saliency_map)
+
+        await send_result(session, {"final_score": final_result})
+
+### video end 체크 ###
+def video_end(message):
+    pass
+
+### saliency map extract ###
+def load_saliency_map():
+    with open('saliency_map.json', 'r') as f:
+        saliency_map_list = json.load(f)
+    return np.array(saliency_map_list)
+
+
+asyncio.run(process_stream())
