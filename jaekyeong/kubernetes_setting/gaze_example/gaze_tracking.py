4b87a7b6a629f30b89861362cb21a5026b1c218c
Pepi10 <anabiotic1@gmail.com>
2024-07-15 10:50:50 +0900

    send result

--- jaekyeong/kubernetes_setting/gaze_example/gaze_tracking.py
+++ jaekyeong/kubernetes_setting/gaze_example/gaze_tracking.py
@@ -205,7 +205,8 @@ def send_result(final_result, video_id, ip_address):
     data = {
         "video_id": video_id,
         "final_score": final_result,
-        "ip_address": ip_address
+        "ip_address": ip_address,
+        "model_type": "gaze_tracking"
     }
     response = requests.post(AGGREGATOR_URL, json=data)
 
