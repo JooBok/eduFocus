585e5a20a23945c3a925062417b69826d416595c
Pepi10 <anabiotic1@gmail.com>
2024-07-16 11:48:14 +0900

    env install

--- env_install/4_k8sCluster_setting.sh
+++ env_install/4_k8sCluster_setting.sh
@@ -2,8 +2,6 @@
 
 ### minikube gpu 실행과 스탠다드 스토리지 클래스를 생성하는 스크립트입니다. ###
 ### 컴퓨터 사양에 맞게 재구성해주세요 ###
-minikube start --gpus nvidia --memory=10240 --cpus=8
+minikube start --memory=10240 --cpus=8
 
 minikube kubectl -- get pods -A
-
-kubectl get storageclass
