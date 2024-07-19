0b09bc5e67e2472380fb216e36ddd6a0bf3c2b46
bok <qhr2291@gmail.com>
2024-07-19 09:41:12 +0900

    c

--- bok/kubernetes/apply.sh
+++ bok/kubernetes/apply.sh
@@ -1,8 +1,8 @@
 # !bin/bash
 
-kubectl apply -f mongodb.yaml
-kubectl apply -f mongodb-express.yaml
+kubectl apply -f secret.yaml
 kubectl apply -f configmap.yaml
 kubectl apply -f pv.yaml
 kubectl apply -f pvc.yaml
-kubectl apply -f secret.yaml
+kubectl apply -f mongodb.yaml
+kubectl apply -f mongodb-express.yaml
