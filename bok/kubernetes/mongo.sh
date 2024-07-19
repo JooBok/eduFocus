c95e0d93355226e0c5f4d25555585ea606a2999f
Pepi10 <anabiotic1@gmail.com>
2024-07-19 20:52:07 +0900

    admin

--- bok/kubernetes/mongo.sh
+++ bok/kubernetes/mongo.sh
@@ -7,6 +7,12 @@ MONGODB_POD=$(kubectl get pods -l app=mongodb -o=jsonpath='{.items[0].metadata.n
 
 # MongoDB에 데이터베이스 및 컬렉션 생성
 kubectl exec -it $MONGODB_POD -- mongo <<EOF
+use admin
+db.createUser({
+	user:"root", 
+	pwd:"root", 
+	roles: [{role: "userAdminAnyDatabase", db: "admin"}, {role: "readWrite", db: "saliency_db"}]
+	})
 use saliency_db
 db.createCollection("$CONTENTS_NAME")
 exit
