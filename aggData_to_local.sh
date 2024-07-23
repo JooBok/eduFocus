308d2c8b7af5642d2356e88ae2e1908c8155dca1
Pepi10 <anabiotic1@gmail.com>
2024-07-23 18:08:33 +0900

    rename

--- aggData_to_local.sh
+++ aggData_to_local.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# 결과 저장 디렉토리 생성
-mkdir -p /home/fp/eduFocus/jaekyeong/kubernetes_setting/aggregator/storage
+### 결과 저장 디렉토리 생성 (위치 원하는 곳으로 변경)###
+mkdir -p /home/fp/eduFocus/jaekyeong/storage
 
-# result-aggregator 파드 이름 가져오기
+### result-aggregator 파드 이름 가져오기 ###
 POD_NAME=$(kubectl get pods -l app=result-aggregator -o jsonpath="{.items[0].metadata.name}")
 
-# 파드에서 호스트로 데이터 복사
-kubectl cp $POD_NAME:/tmp/storage/aggregated_data.csv /home/fp/eduFocus/jaekyeong/kubernetes_setting/aggregator/storage/aggregated_data.csv
+### 파드에서 호스트로 데이터 복사 ###
+kubectl cp $POD_NAME:/tmp/storage/aggregated_data.csv /home/fp/eduFocus/jaekyeong/storage/aggregated_data.csv
 
-# 복사 완료 메시지 출력
 echo "Data copied from pod to host at $(date)"
