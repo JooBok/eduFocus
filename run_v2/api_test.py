bf9ef08738810f577ff78da16260b6e173f6ec2f
Pepi10 <anabiotic1@gmail.com>
2024-07-25 18:35:53 +0900

    test

--- run_v2/api_test.py
+++ run_v2/api_test.py
@@ -8,17 +8,24 @@ def encode(path):
     frame_base64 = base64.b64encode(buffer).decode('utf-8')
     return frame_base64
 
-gw_uri = 'http://192.168.49.2:30942/process'
+### http:// minikube ip : api-gateway nodePort /process
+gw_uri = 'http://192.168.49.2:30942/process' # $
 
-for _ in range(20):
-    if _ < 10:
-        frame_base64 = encode(f'./face_frames/frame_000{_}.png')
-    elif _ >= 10:
-        frame_base64 = encode(f'./face_frames/frame_00{_}.png')
+for _ in range(1800):
+    ### 디렉토리 이름 수정(통일) ###
+    if _ <= 9:
+        frame_base64 = encode(f'./face_frames/frame_000{_}.png') # $ 
+    elif _ <= 99:
+        frame_base64 = encode(f'./face_frames/frame_00{_}.png') # $
+    elif _ <= 999:
+        frame_base64 = encode(f'./face_frames/frame_0{_}.png') # $ 
+    else:
+        frame_base64 = encode(f'./face_frames/frame_{_}.png') # $
 
+    ### metadata 수정 ###
     data = {
-        "video_id": "contents2",
-        "ip_address": "127.0.0.1",
+        "video_id": "contents2", # $
+        "ip_address": "127.0.0.1", # $
         "frame_number": _,
         "last_frame": False,
         "frame": frame_base64
@@ -31,12 +38,13 @@ for _ in range(20):
     )
 print("##########################################################################")
 
-for _ in range(20, 21):
-    frame_base64 = encode(f'./face_frames/frame_00{_}.png')
+for _ in range(1800, 1801):
+    frame_base64 = encode(f'./face_frames/frame_{_}.png')
 
+    ### 위에 맞춰 metadata 수정 ###
     data = {
-        "video_id": "contents2",
-        "ip_address": "127.0.0.1",
+        "video_id": "contents2", # $
+        "ip_address": "127.0.0.1", # $
         "frame_number": _,
         "last_frame": True,
         "frame": frame_base64
