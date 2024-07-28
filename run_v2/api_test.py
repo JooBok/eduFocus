5bb33b0424b1d3995ce3bd8e1a6a04bb6e1baf56
Pepi10 <anabiotic@gmail.com>
2024-07-28 22:39:18 +0900

    new test

--- run_v2/api_test.py
+++ run_v2/api_test.py
@@ -1,30 +1,25 @@
-import base64, cv2, json, requests
+import base64, cv2, json, requests, os
 
 def encode(path):
     print(path)
     frame = cv2.imread(path)
-    frame = cv2.resize(frame, (640, 480))
-    __, buffer = cv2.imencode('.png', frame)
+    frame = cv2.resize(frame, (640, 360))
+    __, buffer = cv2.imencode('.jpg', frame)
     frame_base64 = base64.b64encode(buffer).decode('utf-8')
     return frame_base64
+
+directory = './face_frames'
 
 ### http:// minikube ip : api-gateway nodePort /process
 gw_uri = 'http://192.168.49.2:30942/process' # $
 
-for _ in range(1800):
+for _ in range(1, 300):
     ### 디렉토리 이름 수정(통일) ###
-    if _ <= 9:
-        frame_base64 = encode(f'./face_frames/frame_000{_}.png') # $ 
-    elif _ <= 99:
-        frame_base64 = encode(f'./face_frames/frame_00{_}.png') # $
-    elif _ <= 999:
-        frame_base64 = encode(f'./face_frames/frame_0{_}.png') # $ 
-    else:
-        frame_base64 = encode(f'./face_frames/frame_{_}.png') # $
-
+    file_path = os.path.join(directory, f'frame_{_:04d}.jpg')
+    frame_base64 = encode(file_path)
     ### metadata 수정 ###
     data = {
-        "video_id": "contents2", # $
+        "video_id": "contents1", # $
         "ip_address": "127.0.0.1", # $
         "frame_number": _,
         "last_frame": False,
@@ -38,12 +33,13 @@ for _ in range(1800):
     )
 print("##########################################################################")
 
-for _ in range(1800, 1801):
-    frame_base64 = encode(f'./face_frames/frame_{_}.png')
+for _ in range(300, 301):
+    file_path = os.path.join(directory, f'frame_{_:04d}.jpg')
+    frame_base64 = encode(file_path)
 
     ### 위에 맞춰 metadata 수정 ###
     data = {
-        "video_id": "contents2", # $
+        "video_id": "contents1", # $
         "ip_address": "127.0.0.1", # $
         "frame_number": _,
         "last_frame": True,
